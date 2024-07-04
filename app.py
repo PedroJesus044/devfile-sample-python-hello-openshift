f545357aa757644fafaef203389685d927f67246
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-04 11:59:19 -0600

    Se añaden puntos en el cu cu pumas

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "CU CU PUMAS"
+    return "C.U. C.U. PUMAS"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
