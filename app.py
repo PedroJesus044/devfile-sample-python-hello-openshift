3f41eee5217a838f3257c82f0e9054da8dec2658
Pedro Jesus Ibarra Palmero <52764875+PedroJesus044@users.noreply.github.com>
2024-07-04 12:46:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "C.U. C.U. PUMAS"
+    return "Que chingue su madre el américa"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
